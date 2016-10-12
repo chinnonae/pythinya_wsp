bbb6c5b940a80af1394b0b0eedb85c74414a5ff7
Chinthiti Wisetsombat <chinnonae@gmail.com>
2016-10-12 08:38:40 +0700

    Rename CreateUserSerializer to UserSerializer

--- pythinya/serializers.py
+++ pythinya/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import User
 
 
-class CreateUserSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('email', 'first_name', 'last_name', 'password')
--- pythinya/views.py
+++ pythinya/views.py
@@ -1,6 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.parsers import JSONParser
-from .serializers import CreateUserSerializer
+from .serializers import UserSerializer
 from rest_framework.response import Response
 
 
@@ -8,7 +8,7 @@ class Register(APIView):
     parser_classes = (JSONParser,)
 
     def post(self, request, format=None):
-        user = CreateUserSerializer(data=request.data)
+        user = UserSerializer(data=request.data)
         user.is_valid()
         user.create(user.validated_data)
 
