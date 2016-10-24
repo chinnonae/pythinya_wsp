8812f3c19757883f02c29b623019389c353abf85
TAWEESOFT <taweesoft@TAWEESOFTs-MacBook-Pro.local>
2016-10-24 22:04:57 +0700

    add primary color

---
+++ frontend/containers/theme.js
@@ -0,0 +1,9 @@
+import {getMuiTheme} from 'material-ui/styles';
+import {green300} from 'material-ui/styles/colors';
+var Theme = getMuiTheme({
+  palette : {
+    primary1Color: green300
+  }
+});
+
+export default Theme;
--- frontend/containers/wrapper.jsx
+++ frontend/containers/wrapper.jsx
@@ -1,10 +1,18 @@
+import {RaisedButton, AppBar} from 'material-ui';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import muiTheme from './theme';
+injectTapEventPlugin();
 export default class Wrapper extends React.Component {
-  render () {
-    return (
-      <div>
-        <h1>Hello World</h1>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<MuiThemeProvider muiTheme={muiTheme}>
+				<div>
+          <AppBar title="Pythinya"/>
+				</div>
+			</MuiThemeProvider>
+		);
+	}
 }
-ReactDOM.render(<Wrapper />, document.getElementById('app'));
+ReactDOM.render(
+	<Wrapper/>, document.getElementById('app'));
