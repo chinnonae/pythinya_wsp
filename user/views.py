7ebae99d3e848fd712ab6262bcb4853534178811
Chinthiti Wisetsombat <chinnonae@gmail.com>
2016-10-25 13:33:10 +0700

    add response where register with existing email

--- user/views.py
+++ user/views.py
@@ -4,6 +4,8 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.renderers import JSONRenderer
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework import status
+
 
 from .serializers import UserSerializer
 from .models import User
@@ -15,8 +17,15 @@ class Register(APIView):
     def post(self, request, format=None):
         user = UserSerializer(data=request.data)
         user.is_valid()
-        created = user.create(user.validated_data)
-
+        try:
+            created = user.create(user.validated_data)
+        except KeyError as err:
+            if("email" in err.args):
+                return Response({
+                    "message": "The email has been used"
+                    },
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
         return Response(UserSerializer(created).data)
 
 
