ee2a10629d0a159530ec5873c6968704819c77ed
Chinthiti Wisetsombat <chinnonae@gmail.com>
2016-10-25 12:25:15 +0700

    change url pattern in for user profile

--- user/urls.py
+++ user/urls.py
@@ -7,5 +7,5 @@ from . import views
 urlpatterns = [
     url(r'^signin/$', auth_view.obtain_auth_token),
     url(r'^signup/$', views.Register.as_view()),
-    url(r'^user/profile/(?P<pk>[0-9]+)|()', views.UserView.as_view())
+    url(r'^profile/(?P<pk>[0-9]+)|()$', views.UserView.as_view())
 ]
