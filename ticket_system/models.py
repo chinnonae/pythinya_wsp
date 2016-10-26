704ca3469adc61355ceca20ead711f53d3d016b6
Chinthiti Wisetsombat <chinnonae@gmail.com>
2016-10-26 19:21:31 +0700

    add current_mmr to ticket

--- ticket_system/models.py
+++ ticket_system/models.py
@@ -7,11 +7,13 @@ class Ticket(models):
     min_mmr = models.PositiveIntegerField(_("minimum MMR"))
     max_mmr = models.PositiveIntegerField(_("maximum MMR"))
     booster = models.ForeignKey(User, on_delete=models.CASCADE)
-    client = models.ForeignKey(User, on_delete=models.CASCADE)
-    day_use = models.PositiveIntegerField()
+    client = models.ForeignKey(User, on_delete=models.CASCADE, blank=True)
+    day_used = models.PositiveIntegerField()
     status = models.CharField(choices=(
                                     _("available"),
                                     _("boosting"),
                                     _("waiting for payment"),
                                     _("done"),
                                 ))
+    current_mmr = models.PositiveIntegerField(_("current MMR"), blank=True)
+
