c4d5a57ba2184d3a90e41b7cb070e1b5a1d0fb6e
Chinthiti Wisetsombat <chinnonae@gmail.com>
2016-10-26 19:23:37 +0700

    using simple status code instead of rest framework status boilerplate

--- user/views.py
+++ user/views.py
@@ -3,7 +3,6 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.renderers import JSONRenderer
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework import status
 
 from .serializers import UserSerializer
 from .models import User
@@ -21,15 +20,14 @@ class Register(APIView):
             if("email" in err.args):
                 return Response({
                     "message": "The email has been used",
-                    "status": 400
                     },
-                    status=status.HTTP_400_BAD_REQUEST,
+                    status=400,
                 )
         return Response(
             {
                 "token": str(created.auth_token),
-                "status": 200
-            }
+            },
+            status=200,
         )
 
 
@@ -46,4 +44,4 @@ class UserView(APIView):
 
         serialized = UserSerializer(user)
 
-        return Response(serialized.data)
+        return Response(serialized.data, status=200)
