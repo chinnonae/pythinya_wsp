9b12d0bf0e60298c41dd124369cac7ba4c2ad05e
Chinthiti Wisetsombat <chinnonae@gmail.com>
2016-10-28 09:52:59 +0700

    reconfig ticket system url

--- ticket_system/urls.py
+++ ticket_system/urls.py
@@ -2,9 +2,9 @@ from django.conf.urls import url
 
 from . import views
 
-urlpattern = [
-    url(r'^/&', views.TicketView.as_view()),
-    url(r'^/(?P<pk>[0-9]+)&', views.TicketDetailView.as_view()),
-    url(r'^/(?P<pk>[0-9]+)/buy&', views.TicketPurchaseView.as_view()),
-    url(r'^/(?P<pk>[0-9]+)/progress&', views.TicketProgressView.as_view())
+urlpatterns = [
+    url(r'^$', views.TicketView.as_view()),
+    url(r'^(?P<pk>[0-9]+)$', views.TicketDetailView.as_view()),
+    url(r'^(?P<pk>[0-9]+)/buy$', views.TicketPurchaseView.as_view()),
+    url(r'^(?P<pk>[0-9]+)/progress$', views.TicketProgressView.as_view())
 ]
