0fa7f1aa18047e151f55ea8b7c39b25ac19fa0dd
Chinthiti Wisetsombat <chinnonae@gmail.com>
2016-10-28 08:57:43 +0700

    add entry point for ticket APIs

--- ticket_system/views.py
+++ ticket_system/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
 
-# Create your views here.
+
+class TicketView(APIView):
+
+    def get(self, request):
+        pass
+
+    def post(self, request):
+        pass
+
+
+class TicketDetailView(APIView):
+
+    def get(self, request):
+        pass
+
+
+class TicketPurchaseView(APIView):
+
+    def put(self, request):
+        pass
+
+
+class TicketProgressView(APIView):
+
+    def put(self, request):
+        pass
+
+
+
+
