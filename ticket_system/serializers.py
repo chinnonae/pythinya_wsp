2bbf02dfc3b0986e319f8e53d63477ad1cb171b8
Chinthiti Wisetsombat <chinnonae@gmail.com>
2016-10-28 13:51:00 +0700

    add serializer with custom to_representation()

--- ticket_system/serializers.py
+++ ticket_system/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
 from user.models import User
+from user.serializers import UserSerializer
 from .models import Ticket
 
 
@@ -9,3 +10,14 @@ class TicketSerializer(serializers.ModelSerializer):
     class Meta:
         model = Ticket
         fields = '__all__'
+
+    def to_representation(self, instance):
+        fields = ('min_mmr', 'current_mmr', 'max_mmr', 'booster', 'client', 'day_used', 'status')
+
+        dict = {}
+        for field in fields:
+            value = getattr(instance, field)
+            dict[field] = value
+            if isinstance(value, User):
+                dict[field] = UserSerializer(value).data
+        return dict
