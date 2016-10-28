b73b30089ebc683a3bc4b92bf537bf0da3452011
Chinthiti Wisetsombat <chinnonae@gmail.com>
2016-10-28 20:11:33 +0700

    add id field to TicketSerializer

--- ticket_system/serializers.py
+++ ticket_system/serializers.py
@@ -12,7 +12,7 @@ class TicketSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
     def to_representation(self, instance):
-        fields = ('min_mmr', 'current_mmr', 'max_mmr', 'booster', 'client', 'day_used', 'status')
+        fields = ('id', 'min_mmr', 'current_mmr', 'max_mmr', 'booster', 'client', 'day_used', 'status')
 
         dict = {}
         for field in fields:
