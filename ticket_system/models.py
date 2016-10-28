fb6233466ef9820552829568d4f44e7e3ad2fcb8
Chinthiti Wisetsombat <chinnonae@gmail.com>
2016-10-28 09:45:27 +0700

    add related_name to booster and client, and fix wrong choice configuration

--- ticket_system/models.py
+++ ticket_system/models.py
@@ -3,17 +3,18 @@ from django.utils.translation import ugettext_lazy as _
 
 from user.models import User
 
-class Ticket(models):
+
+class Ticket(models.Model):
     min_mmr = models.PositiveIntegerField(_("minimum MMR"))
     max_mmr = models.PositiveIntegerField(_("maximum MMR"))
-    booster = models.ForeignKey(User, on_delete=models.CASCADE)
-    client = models.ForeignKey(User, on_delete=models.CASCADE, blank=True)
-    day_used = models.PositiveIntegerField()
+    booster = models.ForeignKey(User, on_delete=models.CASCADE, related_name='booster')
+    client = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, related_name='client')
+    day_used = models.PositiveIntegerField(_("day used to boost MMR"))
     status = models.CharField(choices=(
-                                    _("available"),
-                                    _("boosting"),
-                                    _("waiting for payment"),
-                                    _("done"),
-                                ))
+        (_("A"), _("available")),
+        (_("B"), _("boosting")),
+        (_("WP"), _("waiting for payment")),
+        (_("D"), _("done")),
+    ), max_length=2)
     current_mmr = models.PositiveIntegerField(_("current MMR"), blank=True)
 
