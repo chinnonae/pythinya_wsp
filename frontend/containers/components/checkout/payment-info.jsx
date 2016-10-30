8938753d6bd904503fe0a0559194b70f43a9e91e
Thongrapee <kulaspa@gmail.com>
2016-10-30 12:00:47 +0700

    Remove checkbox from table

--- frontend/containers/components/checkout/payment-info.jsx
+++ frontend/containers/components/checkout/payment-info.jsx
@@ -6,18 +6,18 @@ class PaymentInfoPanel extends React.Component {
         <Row>
           <Col xs={12} sm={10} md={10} lg={10} className="text-bold text-right">Price</Col>
           <Col xs={12} sm={2} md={2} lg={2}>
-            <div style={{display: "-webkit-block"}}>
+            <div>
               <div className="text-bold">2000</div>Coins
             </div>
            </Col>
         </Row>
         <Row>
           <Col xs={12} sm={10} md={10} lg={10} className="text-bold text-right">Your balance</Col>
-          <Col s={12} sm={2} md={2} lg={2}>3000 - 2000 Coins</Col>
+          <Col xs={12} sm={2} md={2} lg={2}>3000 - 2000 Coins</Col>
         </Row>
         <Row>
           <Col xs={12} sm={10} md={10} lg={10}></Col>
-          <Col s={12} sm={2} md={2} lg={2}>= 1000 Coins</Col>
+          <Col xs={12} sm={2} md={2} lg={2}>= 1000 Coins</Col>
         </Row>
       </Grid>
     );
