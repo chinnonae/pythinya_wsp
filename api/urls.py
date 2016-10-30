7e6b3e653c72cdf1a32a4be7fadf7081f7135544
Chinthiti Wisetsombat <chinnonae@gmail.com>
2016-10-30 21:30:33 +0700

    move every api to api app

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,25 @@
-from django.conf.urls import url
+from django.conf.urls import url, include
+
+from rest_framework.authtoken import views as auth_view
 
 from . import views
 
-urlpatterns = [
+user_url = [
+    url(r'^signin/$', auth_view.obtain_auth_token),
+    url(r'^signup/$', views.Register.as_view()),
+    url(r'^(?P<pk>[0-9]+/)|()profile/$', views.UserView.as_view())
+]
+
+ticket_url = [
     url(r'^$', views.TicketView.as_view()),
     url(r'^(?P<pk>[0-9]+)$', views.TicketDetailView.as_view()),
     url(r'^(?P<pk>[0-9]+)/buy$', views.TicketPurchaseView.as_view()),
     url(r'^(?P<pk>[0-9]+)/progress$', views.TicketProgressView.as_view()),
     url(r'^(?P<pk>[0-9]+)/cancel$', views.TicketCancelView.as_view()),
     url(r'^(?P<pk>[0-9]+)/complete$', views.TicketCompleteView.as_view())
+]
+
+urlpatterns = [
+    url(r'^user/', include(user_url)),
+    url(r'^ticket/', include(ticket_url))
 ]
