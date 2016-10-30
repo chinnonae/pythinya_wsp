33e4ec1f727a6fb1cfe0982a5e2f40778e1623b0
Thongrapee <kulaspa@gmail.com>
2016-10-30 11:55:57 +0700

    Clean up wrapper, and rearrange components

--- frontend/containers/components/checkout/wrapper.jsx
+++ frontend/containers/components/checkout/wrapper.jsx
@@ -4,29 +4,17 @@ import {Card, CardHeader} from 'material-ui/Card';
 class Wrapper extends React.Component {
   render() {
     var TicketInfoPanel = cc.get('components.checkout.ticketInfoPanel');
+    var PaymentInfoPanel = cc.get('components.checkout.paymentInfoPanel');
     return (
       <Grid className="flex flex-center flex-middle">
         <Grid className="full-width padding-all">
           <TicketInfoPanel />
-          <Row>
-            <Col xs={12} sm={10} md={10} lg={10} className="text-bold">Price</Col>
-            <Col xs={12} sm={2} md={2} lg={2}>
-              <div className="text-bold">2000</div>Coins
-             </Col>
-          </Row>
-          <Row>
-            <Col xs={12} sm={10} md={10} lg={10} className="text-bold">Your balance</Col>
-            <Col s={12} sm={2} md={2} lg={2}>3000 - 2000 Coins</Col>
-          </Row>
-          <Row>
-            <Col xs={12} sm={10} md={10} lg={10}></Col>
-            <Col s={12} sm={2} md={2} lg={2}>= 1000 Coins</Col>
-          </Row>
+          <PaymentInfoPanel />
           <Row>
             <RaisedButton className="pull-right" label="Pay" primary={true} />
           </Row>
           <Row>
-          <p className="pull-right">Cancle this ticket</p>
+          <a className="pull-right">Cancle this ticket</a>
           </Row>
         </Grid>
       </Grid>
