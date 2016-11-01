25d042a3417d9e0b4c9bcff4a603ad788350b7c1
TAWEESOFT <taweesoft@TAWEESOFTs-MacBook-Pro.local>
2016-11-01 11:14:13 +0700

    add close event

--- frontend/containers/components/create-ticket/snackbar.jsx
+++ frontend/containers/components/create-ticket/snackbar.jsx
@@ -1,14 +1,19 @@
 import {Row, Col} from 'react-bootstrap';
 class SnackBar extends React.Component {
+  close() {
+    let element = $('.snackbar-wrapper');
+    element.removeClass('snackbar-wrapper-show');
+  }
 	render() {
 		/* Components */
 		let Input = cc.get('components.input');
 		return (
 			<div className="snackbar-wrapper">
 				<div className="snackbar-container flex">
-					<Col xs={12} sm={12} md={6} lg={6} className="container-center" style={{height: "100%"}}>
+					<Col xs={12} sm={12} md={6} lg={6} className="container-center no-padding" style={{height: "100%"}}>
 						<div className="snackbar padding-all">
 							<b>Create Ticket</b>
+              <img onClick={this.close.bind(this)} src="/assets/close-black.svg" className="pointer pull-top pull-right margin-right margin-top"/>
 							<Row className="flex margin-top">
 								<Col className="container-center" xs={11} sm={10} md={10} lg={10}>
 									<Row>
