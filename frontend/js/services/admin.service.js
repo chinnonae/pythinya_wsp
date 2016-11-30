05ee8b3e50d479d10e19ac185f2c2a52ae922f44
TAWEESOFT <TAWEESOFT@TAWEESOFTs-MacBook-Pro.local>
2016-11-30 23:25:46 +0700

    add admin server

---
+++ frontend/js/services/admin.service.js
@@ -0,0 +1,41 @@
+let services = {
+  getUsers: (callback) => {
+    callback = typeof callback === 'function' ? callback : () => {};
+    let http = getHttp();
+    http.getConstant(http.methods.GET, '/api/admin/list/client', {})
+    .done((res) => {
+      callback(res);
+    })
+    .fail((res) => {
+      console.log(res);
+    });
+  },
+  getVerifiedBoosters: (callback) => {
+    callback = typeof callback === 'function' ? callback : () => {};
+    let http = getHttp();
+    http.getConstant(http.methods.GET, '/api/admin/list/booster', {})
+    .done((res) => {
+      callback(res);
+    })
+    .fail((res) => {
+      console.log(res);
+    });
+  },
+  getPendingBoosters: (callback) => {
+    callback = typeof callback === 'function' ? callback : () => {};
+    let http = getHttp();
+    http.getConstant(http.methods.GET, '/api/admin/list/pending-booster', {})
+    .done((res) => {
+      callback(res);
+    })
+    .fail((res) => {
+      console.log(res);
+    });
+  }
+};
+
+const getHttp = () => {
+  return cc.get('services.http');
+};
+
+cc.register('services.admin', services);
